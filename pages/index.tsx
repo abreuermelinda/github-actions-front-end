06fab47f429592358c33b29ee5c4c19320b1a5c8
ermelinda <ermelinda@ciandt.com>
2023-06-29 11:00:28 -0300

    fix: fixing index file with lint-fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,8 @@ import React from "react";
 
 export default function HomeScreen() {
   return (
-  <div>
-    <h1>Página Inicial</h1>
-    
-  </div>
+    <div>
+      <h1>Página Inicial</h1>
+    </div>
   );
 }
